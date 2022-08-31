f90a8e1a174ef046c7d36d1f6f089ef273b736a2
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-08-31 19:11:25 +0200

    nav bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ul className="Navigation">
+          <li className="Navigation__item"><a href="#About">ABOUT</a></li>
+          <li className="Navigation__item"><a href="#Showcase">SHOWCASE</a></li>
+          <li className="Navigation__item"><a href="#Contacts">CONTACTS</a></li>
+        </ul>
         <h1 className="App-header__h1">
           <strong>Hello</strong> <i>there,</i> <br />
           and <em>welcome</em>.
         </h1>
       </header>
-      <div className="About">
+      <div className="About" id="About">
         <h2 className="About__h2">
           I am a junior front-end developer from <br />
           Vinkovci, Croatia.
@@ -21,12 +26,19 @@ function App() {
           I finished a 3 months course for HTM CSS and React and I am currently searching for a job, while also upgrading mine skills and knowledge in the field of web development, with a smidge of design in it.
         </p>
       </div>
-      <div className="Showcase">
-        <a href="https://github.com/MarioMustapic" target="_blank" rel="noopener noreferre">
+      <div className="Showcase" id="Showcase">
+        <a href="https://mariomustapicchat-app.netlify.app/" target="_blank" rel="noopener noreferrer">
+          <img src= {  } alt="Link to Chat-app" className='chat-app-logo' />
+        </a>
+        <a href="https://github.com/MarioMustapic" target="_blank" rel="noopener noreferrer">
+          <img src= {  } alt="" className='' />
+        </a>
+
+      </div>
+      <div className="Contacts" id="Contacts">
+        <a href="https://github.com/MarioMustapic" target="_blank" rel="noopener noreferrer">
           <img src= { Github } alt="Link to Github" className='github' />
         </a>
-        
-        <a href="https://mariomustapicchat-app.netlify.app/" rel="noreferrer" target="_blank" >Link to chat-app</a>
       </div>
     </div>
   );
